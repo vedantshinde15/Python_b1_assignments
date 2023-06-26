f3cf0998c4f8e948e3c1958fe19fa48a2147e6e0
vedantshinde15 <137713107+vedantshinde15@users.noreply.github.com>
2023-06-26 21:11:57 +0530

    Add files via upload

---
+++ assignment_2.py
@@ -0,0 +1,71 @@
+print("Arithmetic operators")
+a=20
+b=10
+print("addition: ",a+b)
+print("subtraction: ",a-b)
+print("multiplication: ",a*b)
+print("division: ",a/b)
+print("floor division: ",a//b)
+print("exponent: ",a**b)
+print("mod: ",a%b)
+
+print("Assignment operators")
+a=10
+a+=1
+print("+=",a)
+a=10
+a-=1
+print("-=",a)
+a=10
+a*=1
+print("*=",a)
+a=10
+a/=1
+print("/=",a)
+a=10
+a**=1
+print("**=",a)
+a=10
+a//=1
+print("//=",a)
+a=10
+a%=1
+print("%=",a)
+print("comparision operators / Relational operators")
+a=10
+b=5
+print("a>=b ",a>=b)
+print("a<=b ",a<=b)
+print("a>b ",a>b)
+print("a<b ",a<b)
+print("a==b ",a==b)
+print("a!=b ",a!=b)
+
+print("logical operators")
+a=True
+b=False
+print("a and b ",a and b)
+print("a or b",a or b)
+print("not b ",not b)
+ 
+print("Bitwise operators")
+a=10
+b=5
+print("bitwise a & b ",a&b)
+print("bitwise a|b ",a|b)
+print("bitwise a^b ",a^b)
+print("bitwise a>>b ",a>>b)
+print("bitwise a<<b ",a<<b)
+
+
+print("Identity operators")
+a=10
+b=5
+print("a is b ",a is b)
+print("a is not b ",a is not b)
+
+print("membership operators")
+a="Hello"
+print("'H' in a:",'H' in a)
+print("'o' not in a",'o' not in a)
+
